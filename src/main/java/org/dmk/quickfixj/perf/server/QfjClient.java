fa7971f1c7d8b32eae274c3a4953b1c040f0455e
domak <christophe.domas@gmail.com>
2012-07-08 14:21:05 +0200

    implemented basic test: send order/ack

---
+++ src/main/java/org/dmk/quickfixj/perf/server/QfjClient.java
@@ -0,0 +1,123 @@
+package org.dmk.quickfixj.perf.server;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import quickfix.Application;
+import quickfix.DefaultMessageFactory;
+import quickfix.DoNotSend;
+import quickfix.FieldNotFound;
+import quickfix.FileLogFactory;
+import quickfix.FileStoreFactory;
+import quickfix.IncorrectDataFormat;
+import quickfix.IncorrectTagValue;
+import quickfix.LogFactory;
+import quickfix.Message;
+import quickfix.MessageFactory;
+import quickfix.MessageStoreFactory;
+import quickfix.RejectLogon;
+import quickfix.Session;
+import quickfix.SessionID;
+import quickfix.SessionNotFound;
+import quickfix.SessionSettings;
+import quickfix.SocketInitiator;
+import quickfix.UnsupportedMessageType;
+import quickfix.field.ClOrdID;
+import quickfix.field.OrdType;
+import quickfix.field.OrderQty;
+import quickfix.field.Side;
+import quickfix.field.Symbol;
+import quickfix.field.TransactTime;
+import quickfix.fix44.Logon;
+import quickfix.fix44.NewOrderSingle;
+
+public class QfjClient implements Application {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String args[]) throws Exception {
+
+		// FooApplication is your class that implements the Application interface
+		Application application = new QfjClient();
+
+		SessionSettings settings = new SessionSettings(Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream("client.cfg"));
+		MessageStoreFactory storeFactory = new FileStoreFactory(settings);
+		LogFactory logFactory = new FileLogFactory(settings);
+		MessageFactory messageFactory = new DefaultMessageFactory();
+		SocketInitiator initiator = new SocketInitiator(application, storeFactory, settings, logFactory, messageFactory);
+		initiator.start();
+		while (true) {
+			// void
+		}
+	}
+
+	// -------------------------------------------------------------------------
+	private static Logger logger = LoggerFactory.getLogger(QfjClient.class);
+	private int counter;
+	private long sendTime;
+
+	@Override
+	public void onCreate(SessionID sessionId) {
+		logger.debug("on create - session: {}", sessionId);
+		try {
+			Session.sendToTarget(new Logon(), sessionId);
+		} catch (SessionNotFound e) {
+			logger.error("cannot find session", e);
+		}
+	}
+
+	@Override
+	public void onLogon(SessionID sessionId) {
+		logger.debug("on logon - session: {}", sessionId);
+		sendNewOrder(sessionId);
+	}
+
+	@Override
+	public void onLogout(SessionID sessionId) {
+		logger.debug("on logout - session: {}", sessionId);
+
+	}
+
+	@Override
+	public void toAdmin(Message message, SessionID sessionId) {
+		logger.debug("to admin - message: {} - session: {}", message, sessionId);
+	}
+
+	@Override
+	public void fromAdmin(Message message, SessionID sessionId) throws FieldNotFound, IncorrectDataFormat,
+			IncorrectTagValue, RejectLogon {
+		logger.debug("from admin - message: {} - session: {}", message, sessionId);
+
+	}
+
+	@Override
+	public void toApp(Message message, SessionID sessionId) throws DoNotSend {
+		logger.debug("to app - message: {} - session: {}", message, sessionId);
+	}
+
+	@Override
+	public void fromApp(Message message, SessionID sessionId) throws FieldNotFound, IncorrectDataFormat,
+			IncorrectTagValue, UnsupportedMessageType {
+		System.out.println((System.nanoTime() - sendTime) / 1000);
+		logger.debug("from app - message: {} - session: {}", message, sessionId);
+		sendNewOrder(sessionId);
+	}
+
+	private void sendNewOrder(SessionID sessionId) {
+		NewOrderSingle message = new NewOrderSingle(new ClOrdID(Integer.toString(counter++)), new Side(Side.BUY),
+				new TransactTime(new Date()), new OrdType(OrdType.MARKET));
+		message.setString(Symbol.FIELD, "FP");
+		message.setDouble(OrderQty.FIELD, 1);
+		try {
+			sendTime = System.nanoTime();
+			Session.sendToTarget(message, sessionId);
+		} catch (SessionNotFound e) {
+			logger.error("cannot find session", e);
+		}
+	}
+}
---
+++ src/main/java/org/dmk/quickfixj/perf/server/QfjServer.java
@@ -0,0 +1,108 @@
+package org.dmk.quickfixj.perf.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import quickfix.Acceptor;
+import quickfix.Application;
+import quickfix.DefaultMessageFactory;
+import quickfix.DoNotSend;
+import quickfix.FieldNotFound;
+import quickfix.FileLogFactory;
+import quickfix.FileStoreFactory;
+import quickfix.IncorrectDataFormat;
+import quickfix.IncorrectTagValue;
+import quickfix.LogFactory;
+import quickfix.Message;
+import quickfix.MessageFactory;
+import quickfix.MessageStoreFactory;
+import quickfix.RejectLogon;
+import quickfix.Session;
+import quickfix.SessionID;
+import quickfix.SessionNotFound;
+import quickfix.SessionSettings;
+import quickfix.SocketAcceptor;
+import quickfix.UnsupportedMessageType;
+import quickfix.field.AvgPx;
+import quickfix.field.CumQty;
+import quickfix.field.ExecID;
+import quickfix.field.ExecType;
+import quickfix.field.LeavesQty;
+import quickfix.field.OrdStatus;
+import quickfix.field.OrderID;
+import quickfix.field.OrderQty;
+import quickfix.field.Side;
+import quickfix.field.Symbol;
+import quickfix.fix50.ExecutionReport;
+
+public class QfjServer implements Application {
+
+	public static void main(String args[]) throws Exception {
+
+		// FooApplication is your class that implements the Application interface
+		Application application = new QfjServer();
+
+		SessionSettings settings = new SessionSettings(Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream("server.cfg"));
+		MessageStoreFactory storeFactory = new FileStoreFactory(settings);
+		LogFactory logFactory = new FileLogFactory(settings);
+		MessageFactory messageFactory = new DefaultMessageFactory();
+		Acceptor acceptor = new SocketAcceptor(application, storeFactory, settings, logFactory, messageFactory);
+		acceptor.start();
+	}
+
+	// -------------------------------------------------------------------------
+	private static Logger logger = LoggerFactory.getLogger(QfjServer.class);
+	private int counter;
+
+	@Override
+	public void onCreate(SessionID sessionId) {
+		logger.debug("on create - session: {}", sessionId);
+	}
+
+	@Override
+	public void onLogon(SessionID sessionId) {
+		logger.debug("on logon - session: {}", sessionId);
+
+	}
+
+	@Override
+	public void onLogout(SessionID sessionId) {
+		logger.debug("on logout - session: {}", sessionId);
+
+	}
+
+	@Override
+	public void toAdmin(Message message, SessionID sessionId) {
+		logger.debug("to admin - message: {} - session: {}", message, sessionId);
+	}
+
+	@Override
+	public void fromAdmin(Message message, SessionID sessionId) throws FieldNotFound, IncorrectDataFormat,
+			IncorrectTagValue, RejectLogon {
+		logger.debug("from admin - message: {} - session: {}", message, sessionId);
+
+	}
+
+	@Override
+	public void toApp(Message message, SessionID sessionId) throws DoNotSend {
+		logger.debug("to app - message: {} - session: {}", message, sessionId);
+	}
+
+	@Override
+	public void fromApp(Message message, SessionID sessionId) throws FieldNotFound, IncorrectDataFormat,
+			IncorrectTagValue, UnsupportedMessageType {
+		logger.debug("from app - message: {} - session: {}", message, sessionId);
+		Message ack = new ExecutionReport(new OrderID("order-" + counter++), new ExecID(Integer.toString(counter++)),
+				new ExecType(ExecType.NEW), new OrdStatus(OrdStatus.NEW), new Side(message.getChar(Side.FIELD)),
+				new LeavesQty(message.getDouble(OrderQty.FIELD)), new CumQty(message.getDouble(OrderQty.FIELD)));
+		ack.setString(Symbol.FIELD, message.getString(Symbol.FIELD));
+		ack.setDouble(AvgPx.FIELD, 0);
+
+		try {
+			Session.sendToTarget(ack, sessionId);
+		} catch (SessionNotFound e) {
+			logger.error("session not found - id: {}", sessionId);
+		}
+	}
+}
